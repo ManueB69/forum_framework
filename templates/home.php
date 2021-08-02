a32441d16ea094c65ac81b0f4a03b9609a8e74ad
ManueB69 <e.bernardi69@gmail.com>
2021-08-02 18:42:46 +0200

    bettered front-end rending

--- templates/home.php
+++ templates/home.php
@@ -9,14 +9,14 @@ use App\Model\Topic;
     <div id="new_topic">
         <form method="post">
             <input type="text" name="title" placeholder="Titre" />
-            <input type="text-area" name="mess_text" placeholder="Message" />
+            <textarea rows="3" name="mess_text" placeholder="Message" ></textarea>
             <button type="submit" class="btn btn-primary">Ajouter un nouveau sujet</button>
         </form>
     </div>
     <div id="topic_list">
-        <form method="post">
+        <form method="post" id="form_list">
             <input type="hidden" name="id" value="idDuSujet" />
-            <button class="btn btn-link" name="title" value="TitreDuSujet"></button>
+            <button id="moi" class="btn btn-link" name="title" value="TitreDuSujet">Titre du sujet</button>
             <button type="submit" class="btn btn-primary">Modifier</button>
             <button type="submit" class="btn btn-danger">Supprimer</button>
         </form>
