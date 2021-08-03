7f228439d7e7a9c12d7011ff358a94c9903f3d76
ManueB69 <e.bernardi69@gmail.com>
2021-08-03 17:22:05 +0200

    Update header

--- templates/header.php
+++ templates/header.php
@@ -3,23 +3,23 @@
     <nav class="nav">
         <ul>
             <li>
-                <a class="nav-link active" aria-current="page" href="home.php">Accueil</a>
+                <a class="nav-link active" aria-current="page" href="/">Accueil</a>
             </li>
             
             <?php if (empty($_SESSION['user'])): ?>
             <li class="nav-item">
-                <a class="nav-link" href="signUp.php">Créer un compte</a>
+                <a class="nav-link" href="/user">Créer un compte</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="signIn.php">Se connecter</a>
+                <a class="nav-link" href="/signin">Se connecter</a>
             </li>
             
             <?php else: ?> 
             <li class="nav-item">
-                <a class="nav-link" href="modifyUser.php">Modifier mon compte</a>
+                <a class="nav-link" href="/user/">Modifier mon compte</a>
             </li>
             <li class="nav-item">
-                 <a class="nav-link" href="signOut.php">Se dconnecter</a>
+                 <a class="nav-link" href="/">Se dconnecter</a>
             </li>
 
             <?php endif; ?>
